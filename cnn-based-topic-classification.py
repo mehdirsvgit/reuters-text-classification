25a7c73b6f28dfee5cb92455656ee2ef24f59ed8
Mehdi Soufifar <mehdi@companybook.no>
2018-09-19 23:23:21 +0200

    update notebooks. refactoring

--- cnn-based-topic-classification.py
+++ cnn-based-topic-classification.py
@@ -3,9 +3,9 @@ from train import *
 
 data_set = Reuters()
 data_set.load_data('offline')
-for topic in ['earn', 'acq', 'money-fx', 'grain', 'crude', 'trade', 'interest', 'ship', 'wheat', 'corn']:
-    print(
-        "*****************    Training classifier for :  '{}' ******************************************".format(topic))
-    train_positives, train_negatives = data_set.get_text(topic, 'TRAIN')
-    test_positives, test_negatives = data_set.get_text(topic, 'TEST')
-    main(train_positives, train_negatives, test_positives, test_negatives, topic)
+# topics =['earn', 'acq', 'money-fx', 'grain', 'crude', 'trade', 'interest', 'ship', 'wheat', 'corn']:
+
+topic = 'acq'
+train_positives, train_negatives = data_set.get_text(topic, 'TRAIN')
+test_positives, test_negatives = data_set.get_text(topic, 'TEST')
+main(train_positives, train_negatives, test_positives, test_negatives, topic)
